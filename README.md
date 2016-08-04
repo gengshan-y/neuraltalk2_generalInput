bc8793ae32a2406f59d8b929089e5d6d7d2a66c5
Gengshan Yang <gengshan@gpu1.svcl.ucsd.edu>
2016-08-03 18:41:18 -0700

    final version

---
+++ README.md
@@ -0,0 +1,53 @@
+# neuraltalk2_modification
+This is a simplified version of [Neuraltalk2](https://github.com/karpathy/neuraltalk2). 
+It takes features/sentence pairs as input, and uses them to train a language model.
+
+## Data Preparation
+1. Provide path-to-feature/caption pair list in the following json format:  
+[{file_path: 'path/feature1.h5', captions: ['a caption', ...]}, ...]
+
+2. Then call prepro.py to gather training data in an .h5 file and a .json file.
+**Usage**: python prepro.py --input_json path-to-list --num_val max-num-splited-to-val
+**e.g.**   python prepro.py --input_json data/test_input.json --num_val 2
+
+#### output json format
+{
+'ix_to_word'[vocab-index mapping form {ix:'word'}, where ix is 1-indexed]:  
+'feats_info'[a list of pairs stores the split info and feature path]:  
+}
+
+#### output h5 format
+/feats is (N,4096) float32 array of feature data, N is the number of images
+/labels is (M,max_length) uint32 array of encoded labels, zero padded, M is the number of captions
+
+## Train the network
+Then run the training code:
+**Usage:** th train.lua -input_h5 training-h5 -input_json training-json - checkpoint_path output-checkpoint-path -id task-id  
+**e.g.**   th train.lua -input_h5 data/data.h5 -input_json data/data.json -checkpoint_history_path output/ -id 1
+
+## Play around with the code
+#### prepro.py
+  1. It preprocesses the corpus, and assigns the train-val split.
+  
+  2. It also generates a wordCount.txt file in output/ folder that counts the vacab frequency.
+
+  3. Change line 190-194 to modify feature-dimension and feature-name(as stored in hdf5 file)
+
+
+#### misc/DataLoader.lua
+  1. It fetches batches of data before forward pass.
+
+  2. Uncomment line 142-149 to check the correctess of input sentences.
+
+#### train.lua
+  1. It  manages the whole process of training and validation.
+
+  2. In line 176-181, we allocated memory for feature vectors, which avoids memory
+     reallocation after fetching batches of features.
+
+  3. We defined a build_fc() function in "misc/net_utils.lua", and called it in line 130 of train,
+  to build up the connection layer between features LSTM part, instead of a whole cnn model.
+  
+  4. We modified the code to sample sentences during training and valication phase in line 221-225
+  and line 302-312.
+
---
+++ prepro.py
@@ -0,0 +1,234 @@
+####
+ # Filename:        prepro.py
+ # Date:            Jul 09 2016
+ # Last Edited by:  Gengshan Yang
+ # Description:     Gathering files(features.h5, captions) indicated by a .json
+ #                  Then create a .h5 file and a .json file.
+ #                  Input: json file that has the form
+ #                  [{file_path: 'path/img.jpg', captions: ['a caption', ...] 
+ #                  }, ...]
+ ####
+
+"""
+This script reads this json, does some basic preprocessing on the captions
+(e.g. lowercase, etc.), creates a special UNK token, and encodes everything to arrays
+
+Output: a json file, an hdf5 file and a wordCount.txt file
+The hdf5 file contains several fields:
+/feats is (N,4096) uint8 array of feature data
+/labels is (M,max_length) uint32 array of encoded labels, zero padded
+/label_start_ix and /label_end_ix are (N,) uint32 arrays of pointers to the 
+  first and last indices (in range 1..M) of labels for each image
+/label_length stores the length of the sequence for each of the M sequences
+
+The json file has a dict that contains:
+- an 'ix_to_word' field storing the vocab in form {ix:'word'}, where ix is 1-indexed
+- an 'feats_info' field that is a list holding auxiliary information for each image, 
+  such as in particular the 'split' it was assigned to.
+"""
+
+import os
+import json
+import argparse
+from random import shuffle, seed
+import string
+# non-standard dependencies:
+import h5py
+import numpy as np
+from scipy.misc import imread, imresize
+import pdb
+
+def prepro_captions(featPairs):
+  # preprocess all the captions (punctuation, lowercase, special symbols)
+  print 'example processed tokens:'
+  for i,featPair in enumerate(featPairs):
+    featPair['processed_tokens'] = []
+    for j,s in enumerate(featPair['captions']):
+      txt = str(s.encode("utf-8")).lower().translate(None, string.punctuation).strip().split()
+      featPair['processed_tokens'].append(txt)
+      if i < 10 and j == 0: print txt
+
+def build_vocab(featPairs, params):
+  count_thr = params['word_count_threshold']
+
+  # count up the number of words
+  counts = {}
+  for featPair in featPairs:
+    for txt in featPair['processed_tokens']:
+      for w in txt:
+        counts[w] = counts.get(w, 0) + 1
+  cw = sorted([(count,w) for w,count in counts.iteritems()], reverse=True)
+  print 'top words and their counts:'
+  print '\n'.join(map(str,cw[:20]))
+
+  # write an extra file of wordCount 
+  wordCount = '\n'.join(map(str,cw));
+  with open('output/wordCount.txt', 'w') as tmpFile:
+    tmpFile.write(wordCount)
+
+  # print some stats
+  total_words = sum(counts.itervalues())
+  print 'total words:', total_words
+  bad_words = [w for w,n in counts.iteritems() if n <= count_thr]
+  vocab = [w for w,n in counts.iteritems() if n > count_thr]
+  bad_count = sum(counts[w] for w in bad_words)
+  print 'number of bad words: %d/%d = %.2f%%' % (len(bad_words), len(counts), len(bad_words)*100.0/len(counts))
+  print 'number of words in vocab would be %d' % (len(vocab), )
+  print 'number of UNKs: %d/%d = %.2f%%' % (bad_count, total_words, bad_count*100.0/total_words)
+
+  # lets look at the distribution of lengths as well
+  sent_lengths = {}
+  for featPair in featPairs:
+    for txt in featPair['processed_tokens']:
+      nw = len(txt)
+      sent_lengths[nw] = sent_lengths.get(nw, 0) + 1
+  max_len = max(sent_lengths.keys())
+  print 'max length sentence in raw data: ', max_len
+  print 'sentence length distribution (count, number of words):'
+  sum_len = sum(sent_lengths.values())
+  for i in xrange(max_len+1):
+    print '%2d: %10d   %f%%' % (i, sent_lengths.get(i,0), sent_lengths.get(i,0)*100.0/sum_len)
+
+  # lets now produce the final annotations
+  if bad_count > 0:
+    # additional special UNK token we will use below to map infrequent words to
+    vocab.append('UNK')
+    print 'special UNK token inserted'
+  
+  for featPair in featPairs:
+    featPair['final_captions'] = []
+    for txt in featPair['processed_tokens']:
+      caption = [w if counts.get(w,0) > count_thr else 'UNK' for w in txt]
+      featPair['final_captions'].append(caption)
+  return vocab
+
+def assign_splits(featPairs, params):
+  num_val = params['num_val']
+  num_test = params['num_test']
+
+  for i,featPair in enumerate(featPairs):
+      if i < num_val:
+        featPair['split'] = 'val'
+      elif i < num_val + num_test: 
+        featPair['split'] = 'test'
+      else: 
+        featPair['split'] = 'train'
+
+  print 'assigned %d to val, %d to test.' % (num_val, num_test)
+
+def encode_captions(imgs, params, wtoi):
+  """ 
+  encode all captions into one large array, which will be 1-indexed.
+  also produces label_start_ix and label_end_ix which store 1-indexed 
+  and inclusive (Lua-style) pointers to the first and last caption for
+  each image in the dataset.
+  """
+
+  max_length = params['max_length']
+  N = len(imgs)
+  M = sum(len(img['final_captions']) for img in imgs) # total number of captions
+
+  label_arrays = []
+  label_start_ix = np.zeros(N, dtype='uint32') # note: these will be one-indexed
+  label_end_ix = np.zeros(N, dtype='uint32')
+  label_length = np.zeros(M, dtype='uint32')
+  caption_counter = 0
+  counter = 1
+  for i,img in enumerate(imgs):
+    n = len(img['final_captions'])
+    assert n > 0, 'error: some image has no captions'
+
+    Li = np.zeros((n, max_length), dtype='uint32')
+    for j,s in enumerate(img['final_captions']):
+      label_length[caption_counter] = min(max_length, len(s)) # record the length of this sequence
+      caption_counter += 1
+      for k,w in enumerate(s):
+        if k < max_length:
+          Li[j,k] = wtoi[w]
+
+    # note: word indices are 1-indexed, and captions are padded with zeros
+    label_arrays.append(Li)
+    label_start_ix[i] = counter
+    label_end_ix[i] = counter + n - 1
+    
+    counter += n
+  
+  L = np.concatenate(label_arrays, axis=0) # put all the labels together
+  assert L.shape[0] == M, 'lengths don\'t match? that\'s weird'
+  assert np.all(label_length > 0), 'error: some caption had no words?'
+
+  print 'encoded captions to array of size ', `L.shape`
+  return L, label_start_ix, label_end_ix, label_length
+
+def main(params):
+  featPairs = json.load(open(params['input_json'], 'r'))
+  seed(123) # make reproducible
+  shuffle(featPairs) # shuffle the order
+
+  ''' tokenization and preprocessing '''
+  prepro_captions(featPairs)
+
+  ''' create the vocab '''
+  vocab = build_vocab(featPairs, params)
+  itow = {i+1:w for i,w in enumerate(vocab)} # a 1-indexed vocab translation table
+  wtoi = {w:i+1 for i,w in enumerate(vocab)} # inverse table
+
+  ''' assign the splits '''
+  assign_splits(featPairs, params)
+  
+  ''' encode captions in large arrays, ready to ship to hdf5 file '''
+  L, label_start_ix, label_end_ix, label_length = encode_captions(featPairs, params, wtoi)
+
+  ''' create output h5 file '''
+  N = len(featPairs)
+  f = h5py.File(params['output_h5'], "w")
+  f.create_dataset("labels", dtype='uint32', data=L)
+  f.create_dataset("label_start_ix", dtype='uint32', data=label_start_ix)
+  f.create_dataset("label_end_ix", dtype='uint32', data=label_end_ix)
+  f.create_dataset("label_length", dtype='uint32', data=label_length)
+
+  ''' load the feature '''
+  featSet = f.create_dataset("feats", (N, 4096), dtype='float32')
+  for i,featPair in enumerate(featPairs):
+    tmpFeatSet = h5py.File(featPair['file_path'],'r')
+    featSet[i] = tmpFeatSet['mp']  # change here to be consistent with your h5
+
+    if i % 1000 == 0:
+      print 'processing %d/%d (%.2f%% done)' % (i, N, i*100.0/N)
+  f.close()
+  print 'wrote ', params['output_h5']
+
+  ''' create output json file '''
+  out = {}
+  out['ix_to_word'] = itow # encode the (1-indexed) vocab
+  out['feats_info'] = []
+  for i,featPair in enumerate(featPairs):    
+    jfeat = {}
+    jfeat['split'] = featPair['split']
+    jfeat['captions'] = featPair['captions']
+    if 'file_path' in featPair: 
+      jfeat['file_path'] = featPair['file_path']  # copy path over, might need
+    out['feats_info'].append(jfeat)
+
+  json.dump(out, open(params['output_json'], 'w'))
+  print 'wrote ', params['output_json']
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+
+  # input json
+  parser.add_argument('--input_json', required=True, help='input json file to process into hdf5')
+  parser.add_argument('--num_val', required=True, type=int, help='number of images to assign to validation data (for CV etc)')
+  parser.add_argument('--output_json', default='data/data.json', help='output json file')
+  parser.add_argument('--output_h5', default='data/data.h5', help='output h5 file')
+  
+  # options
+  parser.add_argument('--max_length', default=16, type=int, help='max length of a caption, in number of words. captions longer than this get clipped.')
+  parser.add_argument('--word_count_threshold', default=5, type=int, help='only words that occur more than this number of times will be put in vocab')
+  parser.add_argument('--num_test', default=0, type=int, help='number of test images (to withold until very very end)')
+
+  args = parser.parse_args()
+  params = vars(args) # convert to ordinary dict
+  print 'parsed input parameters:'
+  print json.dumps(params, indent = 2)
+  main(params)
